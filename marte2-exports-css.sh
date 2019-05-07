5d383f654d32affdb1d1e6c9bbc7f36faf3e3f79
mdsplus <mdsplus@scdevail.rfx.local>
2019-05-07 17:50:05 +0200

    Working NI6259

---
+++ marte2-exports-css.sh
@@ -0,0 +1,12 @@
+export MARTe2_DIR=~/Projects/MARTe2-dev
+export MARTe2_Components_DIR=~/Projects/MARTe2-components
+#export OPEN62541_LIB=~/Projects/open62541/build/bin
+#export OPEN62541_INCLUDE=~/Projects/open62541/build
+#export EPICS_BASE=~/Projects/epics-base-7.0.2
+#export EPICSPVA=~/Projects/EPICS/base-7.0.2
+#export EPICS_HOST_ARCH=linux-x86_64
+#export PATH=$PATH:$EPICS_BASE/bin/$EPICS_HOST_ARCH
+#export SDN_CORE_INCLUDE_DIR=~/Projects/SDN_1.0.12_nonCCS/src/main/c++/include/
+#export SDN_CORE_LIBRARY_DIR=~/Projects/SDN_1.0.12_nonCCS/target/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_DIR/Build/x86-linux/Core/
+ 
