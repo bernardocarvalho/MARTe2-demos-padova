bb67b35e619b004024a97ba043f95e4531a49f62
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2023-11-23 23:43:49 +0000

    Clear MDS tcl
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- Trees/CreateIsttokMDSplusTree.tcl
+++ Trees/CreateIsttokMDSplusTree.tcl
@@ -55,44 +55,24 @@ add node MEAS1/usage=signal
 add node MEAS1D/usage=signal
 add node MEAS1F/usage=signal
 add node MEAS1FD/usage=signal
-add node MEAS2/usage=signal
-add node MEAS2D/usage=signal
-add node MEAS2F/usage=signal
-add node MEAS2FD/usage=signal
-add node MEAS3/usage=signal
-add node MEAS3D/usage=signal
-add node MEAS3F/usage=signal
-add node MEAS3FD/usage=signal
 
 set def \RTAPPISTTOK::TOP.REFS
 add node REF0/usage=signal
 add node REF0D/usage=signal
 add node REF1/usage=signal
 add node REF1D/usage=signal
-add node REF2/usage=signal
-add node REF2D/usage=signal
-add node REF3/usage=signal
-add node REF3D/usage=signal
 
 set def \RTAPPISTTOK::TOP.ERRORS
 add node ERROR0/usage=signal
 add node ERROR0D/usage=signal
 add node ERROR1/usage=signal
 add node ERROR1D/usage=signal
-add node ERROR2/usage=signal
-add node ERROR2D/usage=signal
-add node ERROR3/usage=signal
-add node ERROR3D/usage=signal
 
 set def \RTAPPISTTOK::TOP.CONTROLS
 add node CONTROL0/usage=signal
 add node CONTROL0D/usage=signal
 add node CONTROL1/usage=signal
 add node CONTROL1D/usage=signal
-add node CONTROL2/usage=signal
-add node CONTROL2D/usage=signal
-add node CONTROL3/usage=signal
-add node CONTROL3D/usage=signal
 
 set def \RTAPPISTTOK::TOP.STATES
 add node STATE0/usage=signal
