71fabaaae421bcac8fc41dd5a809b1f278692395
Bernardo Brotas Carvalho <14007721+bernardocarvalho@users.noreply.github.com>
2023-11-29 23:40:05 +0000

    Update marte2-exports.sh

--- marte2-exports.sh
+++ marte2-exports.sh
@@ -1,7 +1,7 @@
 export MARTe2_DIR=~/Projects/MARTe2-dev
 export MARTe2_Components_DIR=~/Projects/MARTe2-components
-export EPICS_BASE=~/Projects/epics-base-7.0.2
-export EPICSPVA=~/Projects/EPICS/base-7.0.2
+export EPICS_BASE=~/Projects/epics-base
+export EPICSPVA=~/Projects/epics-base
 export EPICS_HOST_ARCH=linux-x86_64
 export PATH=$PATH:$EPICS_BASE/bin/$EPICS_HOST_ARCH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_DIR/Build/x86-linux/Core/:$EPICS_BASE/lib/$EPICS_HOST_ARCH
